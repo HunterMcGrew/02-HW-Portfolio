a9375e60d1cf69f652e4304205325e060aea984a
HunterLM <hunter109@gmail.com>
2022-04-20 08:56:20 -0500

    updated one of the project links, updated some HTML.

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// hamburger navbar functions
+function openNav() {
+  document.getElementById('pancake').style.height = '100%';
+}
+
+function closeNav() {
+  document.getElementById('pancake').style.height = '0%';
+}
+// end hamburger navbar functions
